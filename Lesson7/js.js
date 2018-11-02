48820ed4647422cee1c9c46ca321e4448bb56526
HeiM <blademan82@gmail.com>
2018-11-02 17:51:49 +0200

    input type range=> css generator

---
+++ Lesson7/js.js
@@ -0,0 +1,13 @@
+document.getElementById('radius').oninput = cssGenerator;
+
+function cssGenerator() {
+    var div = document.getElementById('test');
+    
+    //textarea
+    var out = document.getElementById('out');
+//    console.log(this.value);
+    div.style.borderRadius = this.value + 'px';
+    
+    // border-radius: 30px;
+    out.innerHTML = 'border-radius: ' + this.value+'px;' ;
+}
