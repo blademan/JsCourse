7975b6281249bc89089d21ecc7830bcd8b89f4b5
HeiM <blademan82@gmail.com>
2018-11-08 00:02:35 +0200

    timer 60s

---
+++ Lesson22/js.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    
+var div = document.querySelector('.items .item');
+    
+    var timer = new Timer(60, div);
+    
+    setInterval(function(){
+        timer.tick();
+    }, 1000);
+    
+    function Timer(time, elem) {
+        this.time = time;
+        this.elem = elem;
+        
+        this.tick = function() {
+            this.time--;
+            this.elem.innerHTML = this.time;
+        }
+    }
+}
