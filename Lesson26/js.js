4ac20fee907072a6f8c09d15faa8104422158be8
HeiM <blademan82@gmail.com>
2018-11-11 00:41:08 +0200

    game box ver 1.2

--- Lesson26/js.js
+++ Lesson26/js.js
@@ -10,7 +10,7 @@ var timer = new Timer(0, gameTime);
  function activeItem() {
      this.classList.toggle('item--active');
  }
- 
+    
  var runBorder = setInterval(function() {
      var rand = mtRand(0, items.length -1);
      activeItem.call(items[rand]);
@@ -22,17 +22,16 @@ var timer = new Timer(0, gameTime);
     
     for( var i = 0; i < items.length; i++) {
         items[i].onclick = function() {
-    
+           fade(this, 1000, function() {
                 this.style.display = 'none';
                 itemsNone++;
                 
                 if(items.length == itemsNone) {
                alert('Game over: ' + timer.time + 's');
                clearInterval(runBorder);
-               clearInterval(gameTime); 
-                  
+               clearInterval(gameTime);                    
                 }
-           
+           });
         }
     }
     function Timer(time, elem ) {
@@ -50,5 +49,25 @@ var timer = new Timer(0, gameTime);
         return Math.floor(Math.random() *(max -min +1 ) ) ;
     }
     
-  
+    
+    function fade(elem, t, f) {
+        var fps =50;
+        var time = t || 500;
+        var steps =time / fps;
+        var op = 1;
+        var d0 = op /steps;
+        
+        var callback = f || function() {};
+        
+        var timer = setInterval(function () {
+            op -= d0;
+            elem.style.opacity = op;
+            steps--;
+            
+            if ( steps === 0 ) {
+                clearInterval(timer);
+                callback.call(elem);
+            }
+        }, (1000 /fps));
+    } 
 }
