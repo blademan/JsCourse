ef8727066dc66857222ad61941bd17c16781a2ea
HeiM <blademan82@gmail.com>
2018-11-13 19:16:08 +0200

    forma

--- Lesson28/js.js
+++ Lesson28/js.js
@@ -1,24 +1,54 @@
 window.onload = function(e) {
     
     var input = document.querySelectorAll('form input');
-    
+  
    document.querySelector('form').onsubmit = function(e) {
        var error = false;
+      
        
        for (var i = 0; i<input.length; i++) {
           if(input[i].value === ''){
-                 input[i].classList.add('errorInput');
-              error = true;
+            input[i].classList.add('errorInput');
+            error = true;
              
            
-          } else { 
-              input[i].classList.remove('errorInput');
-                   }
+          } 
+       }
+       
+       
            if(error) {
                 e.preventDefault();
            }
+       
+   }
+          
+   
+   
+   for ( var i = 0; i< input.length; i++){
+               
+           input[i].oninput = function(){
+               
+           
+              this.classList.remove('errorInput');
+           
+           }
+       }
+           
+       for ( var i = 0; i< input.length; i++){               
+           input[i].onfocus = function(){            
+            this.classList.add('focus');
+           
+           }
+       }   
+    for ( var i = 0; i< input.length; i++){               
+           input[i].onblur = function(){            
+            this.classList.remove('focus');
+           
+           }
+       }
+           
+       
            
        }
-   }
+   
   
-}
