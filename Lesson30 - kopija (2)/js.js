4bd2d831073781c6fb785c7e3b3e5735fd1f9b38
HeiM <blademan82@gmail.com>
2018-11-16 10:40:28 +0200

    upd

--- Lesson30 - kopija (2)/js.js
+++ Lesson30 - kopija (2)/js.js
@@ -1,9 +1,24 @@
 $(function() {
     
+    
+    $('.faq .answer:first').show();
     $('.faq .ask').on('click', function() {
         var answer = $(this).next();
         
         $('.faq .answer').not(answer).slideUp(400);
        answer.slideToggle(500) ;
     });
+    
+    
+    $('.list .jautajums').on('click', function() {
+       var atbilde = $(this).next();
+        $('.list .atbilde').not(atbilde).slideUp(300); 
+        atbilde.slideToggle(500);
+       
+    });
+    
+    
+    
+    
+    
 })
