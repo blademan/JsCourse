0faa672078f305dbad17308f535bf2cef3285653
HeiM <blademan82@gmail.com>
2018-11-17 12:46:29 +0200

    real time clock

---
+++ Lesson30 - kopija (5)/js.js
@@ -0,0 +1,10 @@
+window.onload = displayTime()
+    
+function displayTime() {
+    var now = new Date();
+    var div = document.querySelector('.clock');
+    
+    div.innerHTML = now.toLocaleTimeString();
+    
+    setTimeout(displayTime, 1000);
+}
